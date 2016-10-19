c027f48a41fe98ec8d1748d3476edeb5c7bcde62
vovesja2 <vovesja2@J09-13.uhk.cz>
2016-10-19 16:12:12 +0200

    Založení třídy Game

--- src/cz/uhk/fim/pro2/game/Game.java
+++ src/cz/uhk/fim/pro2/game/Game.java
@@ -1,10 +1,12 @@
+package cz.uhk.fim.pro2.game;
 
-public class Main {
+public class Game {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	System.out.println("Fim Bird");
 
-	  System.out.println("Ahoj ");
 	}
 
 }
