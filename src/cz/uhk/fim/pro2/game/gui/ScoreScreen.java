18b6ee14be3bbc2a81b8b64b973f5b26140306e9
voves5 <VovJak96>
2016-12-19 22:59:40 +0100

    finisScreen,ScoreIthem

--- src/cz/uhk/fim/pro2/game/gui/ScoreScreen.java
+++ src/cz/uhk/fim/pro2/game/gui/ScoreScreen.java
@@ -1,5 +1,7 @@
 package cz.uhk.fim.pro2.game.gui;
 
+import cz.uhk.fim.pro2.game.ScoreManager;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,17 +11,22 @@ public class ScoreScreen extends Screen {
 
 	public ScoreScreen(MainFrame mainFrame) {
 		super(mainFrame);
-		
-		JButton jButtonBack = new JButton("Back");
-		
-	
-		 jButtonBack.addActionListener(new ActionListener() {
+
+		for (int i = 0; i < ScoreManager.getItemNumber(); i++) {
+			int score = ScoreManager.getItemIndex(i);
+			Scoreithemm scoreItem = new Scoreithemm(i+1,score);
+			scoreItem.setBounds(50,200+i*50,300,50);
+
+			add(scoreItem);
+
+		}
+				ScoreManager.getList();
+				JButton jButtonBack = new JButton("Back");
+				 jButtonBack.addActionListener(new ActionListener() {
 				
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					mainFrame.setScreen(new HomeScreen(mainFrame));
-					
-					
 				}
 			});
 	add(jButtonBack);	
